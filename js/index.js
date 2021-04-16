1c3648bc62e60e1045cb9cc41bb772e263ab9820
Tran Duong <dtran2108@gmail.com>
2021-04-16 16:59:00 +0700

    add promotion

--- js/index.js
+++ js/index.js
@@ -1,3 +1,18 @@
 function toggleDropdown() {
   document.getElementById("userDropdown").classList.toggle("show");
 }
+
+function copyToClipboard() {
+  var copyText = document.getElementById("couponCode");
+  copyText.select();
+  copyText.setSelectionRange(0, 99999);
+  document.execCommand("copy");
+  
+  var tooltip = document.getElementById("couponCode");
+  tooltip.innerHTML = "Copied: " + copyText.value;
+}
+
+function outFunc() {
+  var tooltip = document.getElementById("couponCode");
+  tooltip.innerHTML = "Copy to clipboard";
+}
