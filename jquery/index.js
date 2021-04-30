8fc00adfbb887bd7a73cc1f911fed03ca7e0384d
Tran Duong <dtran2108@gmail.com>
2021-04-30 15:20:20 +0700

    fix minor bugs

--- jquery/index.js
+++ jquery/index.js
@@ -1,3 +1,41 @@
+$(document).ready(function () {
+  $('.chain-slick').slick({
+    dots: true,
+    infinite: false,
+    speed: 300,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    responsive: [
+      {
+        breakpoint: 1440,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+  });
+});
+
 $('.add-to-cart').on('click', function () {
   showHeader('simplifiedHeader');
   var cart = $('.shopping-cart');
